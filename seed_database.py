1cb42569c32c73067fd47a57eba6a15faba17e1d
Your Name <maddykmckeown@gmail.com>
2024-04-18 16:33:58 -0600

    git add

---
+++ seed_database.py
@@ -0,0 +1,104 @@
+import os
+import json
+from random import choice, randint
+from datetime import datetime
+from model import db, User, connect_to_db
+
+
+import model
+
+import requests
+
+# res = requests.get('https://random-word-api.vercel.app/')
+
+# search_results = res.json()
+# app.app_context().push()
+# model.connect_to_db(app)
+# model.db.create_all()
+
+
+
+
+
+
+
+def get_word_list(word_length):
+    url = "https://random-word-api.vercel.app/api"
+    params = {
+        "words": 500,
+        "length": word_length
+    }
+    # https://random-word-api.vercel.app/api?words=500&length=5
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        word_list = response.json()
+        for word in word_list: # word_list = ["bingo", "trunk", "lapse"......]
+            new_word = model.Word(word_id=word, letter_count=word_length, word_score=word_length) # word_id, letter_count, word_score
+             # Assuming Word has a 'word' attribute
+            # check if word already in database
+            if model.Word.query.get(word) is None: #checks if word in database already
+                model.db.session.add(new_word)
+                model.db.session.commit()
+        return word_list
+    else:
+        return []
+
+
+# word_list_5 = get_word_list(5)
+# word_list_6 = get_word_list(6)
+# word_list_7 = get_word_list(7)
+# print(get_word_list)
+
+
+# for n in range(10):
+#     email = f'user{n}@test.com' 
+#     password = 'test'
+
+
+
+def create_user(email, password, user_tokens=0, user_level=1):
+    """Creates an instance of a User
+    
+    Note: Creates User object from model.py
+    """
+    # email and password come from server where we call the create_user() function
+    user = model.User(email=email, password=password, user_tokens=user_tokens, user_level=user_level)
+
+    return user
+
+
+def get_users():
+
+
+    return User.query.all()
+
+    # create a user => email, password (user_tokens, user_level)
+    # user = model.User() # todo fill this in
+
+    # You don't need a crud file
+    # user = crud.create_user(email, password)
+    model.db.session.add(user)
+
+    for _ in range(10):
+        word_id = choice(Word)
+        score = int(1, 5)  #level?
+
+
+    model.db.session.commit()
+
+
+if __name__ == '__main__':
+    from server import app
+
+    app.app_context().push()
+    model.connect_to_db(app)
+
+    word_list_5 = get_word_list(5)
+    word_list_6 = get_word_list(6)
+    word_list_7 = get_word_list(7)
+    print(get_word_list)
+
+    for n in range(10):
+        email = f'user{n}@test.com' 
+        password = 'test'
+        print(email, password)
---
+++ server.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request, flash, session, redirect
+from model import connect_to_db, db
+# import crud
+# import seed_database as seed
+from seed_database import create_user
+from jinja2 import StrictUndefined
+
+# creating instance of a flask app
+app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def home_page():
+    if 'name' in session:
+        return redirect('/top-melons')
+    return render_template('homepage.html')
+
+# Model ~ View ~ Controller
+@app.route('/some-url', methods=['GET', 'POST']) # method that creates a route/URL/path; methods means what type of request
+def get_some_page_name(): # view function, view meaning it renders something to the browser
+    # return something a browser can understand
+    # browsers can understand text, html, json
+    # return render_template('some-html.html')
+    # return redirect('/some-other-url') # redirecting to another url (@app.route) that renders a template
+    return ''
+
+@app.route("/users", methods=["POST"])
+def register_user():
+
+    # POST == request.form => python dicts containing key/value pairs
+    # GET == request.args => python dicts containing key/value pairs
+    email = request.form.get("email") # request.form['email']
+    password = request.form.get("password")
+
+    # user = seed.create_user(email, password)
+    user = create_user(email, password) # returns an instance of a User object
+    # For login =>> User.query.filter(email==email) # querying users table for the user with that email and password
+    if user:
+        flash("Cannot create an account with that email. Try again.")
+    else:
+        # user = seed.create_user(email, password)
+        # adding user to database
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created! Please log in.")
+
+    return redirect("/login")
+    # return redirect("/login")
+
+@app.route("/login", methods=['GET'])
+def login_page():
+    return render_template("login.html")
+
+@app.route("/user_login", methods=['POST'])
+def user_login_page():
+    email = request.form.get("email") # request.form['email']
+    password = request.form.get("password")
+
+    user = User.query.filter(User.email == email).first()
+ 
+    if not user or user.password != password:
+        flash("The email or password you entered was incorrect.")
+    else:
+        # Log in user by storing the user's email in session
+        session["user_email"] = user.email
+        flash(f"Welcome back, {user.email}!")
+
+    return redirect("/")
+    # return redirect ("/")
+
+    # processing login (ratings lab has good examples)
+    # checking if user exists => User.query.filter(...) # querying users table for the user with that email and password
+    # add user to flask session => reference login session from ratings lab
+
+    # return render_template()
+    
+
+
+
+
+if __name__ == '__main__':
+    connect_to_db(app)
+    app.run(host='0.0.0.0', debug=True)
